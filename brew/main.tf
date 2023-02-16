634447cc6deb75f91019409efcbdb6b6ae1a02f4
Alexandros Papageorgiou Koufidis <alexandros@miro.com>
2023-02-16 17:23:34 +0100

    Create brew module and add casks

---
+++ brew/main.tf
@@ -0,0 +1,41 @@
+locals {
+  brew_packages = [
+    "aws-vault",
+    "--cask alt-tab",
+    "--cask docker",
+    "--cask raycast",
+    "fzf",
+    "fig",
+    "gh",
+    "gitleaks",
+    "httpie",
+    "k9s",
+    "kubectl",
+    "kubectx",
+    "lazygit",
+    "micro",
+    "pinentry-mac",
+    "starship",
+    "terraform-docs",
+    "zoxide",
+  ]
+  cmd_print_version = jsonencode({ "\"version\"" = "\"$(brew list --versions $PACKAGE)\"" })
+}
+
+resource "shell_script" "brew_package" {
+  for_each = toset(local.brew_packages)
+
+  lifecycle_commands {
+    create = format("brew install -q $PACKAGE; echo %s", local.cmd_print_version)
+    read   = format("echo %s", local.cmd_print_version)
+    update = format("brew install -q $PACKAGE; echo %s", local.cmd_print_version)
+    delete = "brew uninstall -q $PACKAGE"
+  }
+
+  environment = {
+    HOMEBREW_NO_INSTALL_UPGRADE            = 1
+    HOMEBREW_NO_ENV_HINTS                  = 1
+    HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK = 1
+    PACKAGE                                = each.value
+  }
+}
